bae0acc25aeae0eec156a5639b686287d3f3a015
Bishwajitkuat <bishwajitkuat@gmail.com>
2023-09-14 23:32:07 +0300

    script added

---
+++ createSettingsPhp.sh
@@ -0,0 +1,2 @@
+cp drupal/web/sites/default/default.settings.php drupal/web/sites/default/settings.php
+cp example.gitignore .gitignore
---
+++ drupalInstallScript.sh
@@ -0,0 +1,5 @@
+sudo apt-get install php-xml
+sudo apt-get install php-gd
+sudo apt-get install php-intl
+composer create-project drupal/recommended-project drupal
+docker compose up -d
